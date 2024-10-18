2181b83c8993669e942cee217b6be12cd115d89f
Hasna <151807926+anakabif4@users.noreply.github.com>
2024-10-18 18:46:26 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Android\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\PrakMob\\keuangan_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Android\\flutter"
+  "PROJECT_DIR=C:\\PrakMob\\keuangan_app"
+  "FLUTTER_ROOT=D:\\Android\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\PrakMob\\keuangan_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\PrakMob\\keuangan_app"
+  "FLUTTER_TARGET=C:\\PrakMob\\keuangan_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\PrakMob\\keuangan_app\\.dart_tool\\package_config.json"
+)
